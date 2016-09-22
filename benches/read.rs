74d684faf490e604c741249b0e3dcd47fba18584
meh <meh@schizofreni.co>
2016-09-23 01:48:49 +0200

    benches: add benchmarks for other formats

--- benches/read.rs
+++ benches/read.rs
@@ -26,6 +26,29 @@ mod png {
 	}
 }
 
+mod jpeg {
+	mod read {
+		use test::Bencher;
+		use image;
+		use picto;
+
+		#[bench]
+		fn image(b: &mut Bencher) {
+			b.iter(|| image::open("tests/rainbow.jpeg").unwrap());
+		}
+
+		#[bench]
+		fn picto_with_convert(b: &mut Bencher) {
+			b.iter(|| picto::read::from_path::<u8, picto::color::Rgba, _>("tests/rainbow.jpeg").unwrap());
+		}
+
+		#[bench]
+		fn picto_as_is(b: &mut Bencher) {
+			b.iter(|| picto::read::from_path::<u8, picto::color::Rgb, _>("tests/rainbow.jpeg").unwrap());
+		}
+	}
+}
+
 mod bmp {
 	mod read {
 		use test::Bencher;
@@ -45,6 +68,46 @@ mod bmp {
 		#[bench]
 		fn picto_as_is(b: &mut Bencher) {
 			b.iter(|| picto::read::from_path::<u8, picto::color::Rgb, _>("tests/rainbow.bmp").unwrap());
+		}
+	}
+}
+
+mod tga {
+	mod read {
+		use test::Bencher;
+		use image;
+		use picto;
+
+		#[bench]
+		fn image(b: &mut Bencher) {
+			b.iter(|| image::open("tests/rainbow.tga").unwrap());
+		}
+
+		#[bench]
+		fn picto_with_convert(b: &mut Bencher) {
+			b.iter(|| picto::read::from_path::<u8, picto::color::Rgba, _>("tests/rainbow.tga").unwrap());
+		}
+
+		#[bench]
+		fn picto_as_is(b: &mut Bencher) {
+			b.iter(|| picto::read::from_path::<u8, picto::color::Rgb, _>("tests/rainbow.tga").unwrap());
+		}
+	}
+}
+
+mod xyz {
+	mod read {
+		use test::Bencher;
+		use picto;
+
+		#[bench]
+		fn picto_with_convert(b: &mut Bencher) {
+			b.iter(|| picto::read::from_path::<u8, picto::color::Rgba, _>("tests/boat.xyz").unwrap());
+		}
+
+		#[bench]
+		fn picto_as_is(b: &mut Bencher) {
+			b.iter(|| picto::read::from_path::<u8, picto::color::Rgb, _>("tests/boat.xyz").unwrap());
 		}
 	}
 }
