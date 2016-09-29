0cbcecb803de57ac2c8cf4df11db9e3b61b1929a
meh <meh@schizofreni.co>
2016-09-29 19:11:59 +0200

    examples: use clap

--- examples/blur.rs
+++ examples/blur.rs
@@ -1,12 +1,46 @@
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyleft (ↄ) meh. <meh@schizofreni.co> | http://meh.schizofreni.co
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+extern crate clap;
+use clap::{App, Arg};
+
 extern crate picto;
 use picto::color::Rgba;
 use picto::processing::prelude::*;
 
-use std::env;
-
 fn main() {
-	let image = picto::read::from_path::<u8, Rgba, _>(env::args().nth(1).unwrap()).unwrap();
-	let by    = env::args().nth(2).unwrap().parse::<f32>().unwrap();
+	let matches = App::new("blur")
+		.version(env!("CARGO_PKG_VERSION"))
+		.about("Resize an image.")
+		.arg(Arg::with_name("INPUT")
+			.index(1)
+			.required(true)
+			.help("Path to the input image."))
+		.arg(Arg::with_name("OUTPUT")
+			.index(2)
+			.required(true)
+			.help("Path to the output image"))
+		.arg(Arg::with_name("by")
+			.short("b")
+			.long("by")
+			.takes_value(true)
+			.required(true)
+			.help("The amount to blur by."))
+		.get_matches();
 
-	picto::write::to_path(env::args().nth(3).unwrap(), &image.blur::<u8, Rgba>(by)).unwrap();
+	let image = picto::read::from_path::<u8, Rgba, _>(matches.value_of("INPUT").unwrap()).unwrap();
+	let by    = matches.value_of("by").unwrap().parse::<f32>().unwrap();
+
+	picto::write::to_path(matches.value_of("OUTPUT").unwrap(), &image.blur::<u8, Rgba>(by)).unwrap();
 }
--- examples/resize.rs
+++ examples/resize.rs
@@ -1,14 +1,54 @@
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyleft (ↄ) meh. <meh@schizofreni.co> | http://meh.schizofreni.co
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+extern crate clap;
+use clap::{App, Arg};
+
 extern crate picto;
 use picto::color::Rgba;
 use picto::processing::prelude::*;
 
-use std::env;
-
 fn main() {
-	let image   = picto::read::from_path::<u8, Rgba, _>(env::args().nth(1).unwrap()).unwrap();
-	let scaler  = env::var("SCALER").unwrap_or("nearest".into()).to_lowercase();
-	let by      = env::args().nth(2).unwrap().parse::<f32>().unwrap();
-	let resized = match &*scaler {
+	let matches = App::new("resize")
+		.version(env!("CARGO_PKG_VERSION"))
+		.about("Resize an image.")
+		.arg(Arg::with_name("INPUT")
+			.index(1)
+			.required(true)
+			.help("Path to the input image."))
+		.arg(Arg::with_name("OUTPUT")
+			.index(2)
+			.required(true)
+			.help("Path to the output image"))
+		.arg(Arg::with_name("by")
+			.short("b")
+			.long("by")
+			.takes_value(true)
+			.required(true)
+			.help("The amount to scale by."))
+		.arg(Arg::with_name("scaler")
+			.short("s")
+			.long("scaler")
+			.takes_value(true)
+			.validator(is_scaler)
+			.help("The scaling algorithm to use (nearest, linear, cubic, gaussian, lanczos2, lanczos3, super-xbr). (default is `nearest`)"))
+		.get_matches();
+
+	let image = picto::read::from_path::<u8, Rgba, _>(matches.value_of("INPUT").unwrap()).unwrap();
+	let by    = matches.value_of("by").unwrap().parse::<f32>().unwrap();
+
+	picto::write::to_path(matches.value_of("OUTPUT").unwrap(), &match &*matches.value_of("scaler").unwrap_or("nearest").to_lowercase() {
 		"nearest" =>
 			image.scale_by::<scaler::Nearest, u8, Rgba>(by),
 
@@ -31,8 +71,22 @@ fn main() {
 			image.scale_by::<scaler::xbr::Super, u8, Rgba>(by),
 
 		_ =>
-			panic!("unknown scaler")
-	};
+			unreachable!()
+	}).unwrap();
+}
 
-	picto::write::to_path(env::args().nth(3).unwrap(), &resized).unwrap();
+fn is_scaler(arg: String) -> Result<(), String> {
+	match &*arg.to_lowercase() {
+		"nearest"   |
+		"linear"    |
+		"cubic"     |
+		"gaussian"  |
+		"lanczos2"  |
+		"lanczos3"  |
+		"super-xbr" =>
+			Ok(()),
+
+		_ =>
+			Err("unknown scaler".into())
+	}
 }
